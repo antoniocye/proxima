b9b7bc6f1cd94f2298ad08fcc230fa3a92d74f7e
Antonio Kambiré <antoniokambire@gmail.com>
2024-02-17 01:35:03 -0800

    Basic functionalities of the Profile class. Basic auth functions and initialization of firebase modules done in Init.js

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # proxima
 Dating made easy.
+
+## Dependencies
+- Firebase, run as admin, under \app
+`npm install -g firebase-tools`
+`npm install firebase`
--- app/App.js
+++ app/App.js
@@ -1,7 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Profile from './database/Profile.js';
+import { init, auth, db } from './database/Init.js'
 
 export default function App() {
+  for(i = 0; i < 5; i++){
+    console.log("----------------------------------------");
+  }
+  
+  result = init();
+  user = new Profile({name:"Antonio", email:"antoniokambire@gmail.com", password:"Hello123"});
+  
+  console.log("We have finished profile creation");
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
